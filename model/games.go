c0d5c0d1e01a7e8f92df6c8c0a85675325e4bc84
Sebastien Carlier <s.carlier@epixode.fr>
2018-09-30 09:13:28 +0000

    model: add game_player queries

--- model/games.go
+++ model/games.go
@@ -22,6 +22,16 @@ type Game struct {
   Task_params string /*json*/
 }
 
+type GamePlayer struct {
+  Game_id string
+  Rank int
+  Team_id string
+  Team_player int
+  Created_at time.Time
+  Updated_at time.Time
+  Commands string
+}
+
 func (m *Model) addGame(key string, gameParams string, taskParams string, firstBlock string) error {
   var err error
   _, err = m.db.Exec(
@@ -58,6 +68,40 @@ func (m *Model) setPlayerCommands (gameKey string, teamId string, teamPlayer int
   return nil
 }
 
+func (m *Model) loadTeamCommands (gameKey string, teamId string, f Facets) ([]GamePlayer, error) {
+  var err error
+  rows, err := m.db.Queryx(
+    `SELECT gp.* FROM game_players gp
+      INNER JOIN games g ON gp.game_id = g.id
+      WHERE g.game_key = ? AND gp.team_id = ?
+      ORDER BY rank`,
+    gameKey, teamId)
+  if err != nil { return nil, errors.Wrap(err, 0) }
+  defer rows.Close()
+  var items []GamePlayer
+  for rows.Next() {
+    item, err := m.loadGamePlayerRow(rows, f)
+    if err != nil { return nil, err }
+    items = append(items, *item)
+  }
+  return items, nil
+}
+
+func (m *Model) getGameCommands (gameId string) ([]GamePlayer, error) {
+  var err error
+  rows, err := m.db.Queryx(
+    `SELECT * FROM game_players gp WHERE game_id = ? ORDER BY rank`, gameId)
+  if err != nil { return nil, errors.Wrap(err, 0) }
+  defer rows.Close()
+  var items []GamePlayer
+  for rows.Next() {
+    item, err := m.loadGamePlayerRow(rows, NullFacet)
+    if err != nil { return nil, err }
+    items = append(items, *item)
+  }
+  return items, nil
+}
+
 func (m *Model) loadGame(key string, f Facets) (*Game, error) {
   return m.loadGameRow(m.db.QueryRowx(
     `SELECT * FROM games WHERE game_key = ?`, key), f)
@@ -77,6 +121,22 @@ func (m *Model) loadGameRow(row IRow, f Facets) (*Game, error) {
     nullTimeProp(view, "roundEndsAt", res.Round_ends_at)
     view.Prop("firstBlock", j.String(res.First_block))
     view.Prop("currentBlock", j.String(res.Current_block))
+  }
+  return &res, nil
+}
+
+func (m *Model) loadGamePlayerRow(row IRow, f Facets) (*GamePlayer, error) {
+  var res GamePlayer
+  err := row.StructScan(&res)
+  if err == sql.ErrNoRows { return nil, nil }
+  if err != nil { return nil, errors.Wrap(err, 0) }
+  if f.Base {
+    view := j.Object()
+    view.Prop("rank", j.Int(res.Rank))
+    view.Prop("teamPlayer", j.Int(res.Team_player))
+    timeProp(view, "createdAt", res.Created_at)
+    timeProp(view, "updatedAt", res.Updated_at)
+    view.Prop("commands", j.String(res.Commands))
   }
   return &res, nil
 }
