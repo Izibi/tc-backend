f01961867655ae1914c21235780fdf6cff46221c
Sebastien Carlier <s.carlier@epixode.fr>
2018-10-09 09:19:29 +0000

    clean up

--- games/routes.go
+++ games/routes.go
@@ -110,24 +110,6 @@ func SetupRoutes(r gin.IRoutes, newApi utils.NewApi, config Config, store *block
     api.Result(j.Null)
   })
 
-  r.POST("/Games/:gameKey/Execute", func (c *gin.Context) {
-    api := newApi(c)
-    gameKey := c.Param("gameKey")
-    m := model.New(c, db)
-    game, err := m.LoadGame(gameKey, model.NullFacet)
-    if err != nil { api.Error(err); return }
-    if !game.Locked { api.StringError("game is not locked"); return }
-    // TODO: make the block on a remote server
-    newBlock, err := store.MakeCommandBlock(game.Last_block, game.Next_block_commands)
-    if err != nil { api.Error(err); return }
-    err = m.EndRoundAndUnlock(gameKey, newBlock)
-    if err != nil { api.Error(err); return }
-    es.Publish(gameChannel(gameKey), newBlockMessage(newBlock))
-    res := j.Object()
-    res.Prop("next_block", j.String(newBlock))
-    api.Result(res)
-  })
-
   r.POST("/Games/:gameKey/Ping", func (c *gin.Context) {
     err := es.Publish(c.Param("gameKey"), "ping")
     if err != nil {
